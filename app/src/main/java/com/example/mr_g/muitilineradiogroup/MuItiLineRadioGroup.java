31836e0ac793f5e50f9cf807ab5b9c5f1bce011d
gaoyiming <787080436@qq.com>
2016-08-18 17:23:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mr_g/muitilineradiogroup/MuItiLineRadioGroup.java
@@ -0,0 +1,32 @@
+package com.example.mr_g.muitilineradiogroup;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by Mr_g on 16/8/18.
+ */
+
+public class MuItiLineRadioGroup extends ViewGroup {
+    public MuItiLineRadioGroup(Context context) {
+        super(context);
+    }
+
+    public MuItiLineRadioGroup(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MuItiLineRadioGroup(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public MuItiLineRadioGroup(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
